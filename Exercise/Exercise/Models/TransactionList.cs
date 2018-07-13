0d9ac9b80d4a9e026ffb0d06fea3fc7d4b195f6a
bdsipon <sipon.here@gmail.com>
2018-07-14 08:55:27 +1000

    Build the domain model for tranactionlist

---
+++ Exercise/Exercise/Models/TransactionList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise.Models
+{
+    public class TransactionList
+    {
+        public string accountNumber { get; set; }
+        public string tranactionType { get; set; }
+        public string amount { get; set; }
+        public string balance { get; set; }
+        public string description { get; set; }
+        public string tranaction_date { get; set; }
+    }
+}
